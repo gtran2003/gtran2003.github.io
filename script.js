4f5def26efe5e3c56030e519e7c7e8b4d49ff884
gtran2003 <gtran2@cougarnet.uh.edu>
2026-06-28 17:25:10 -0500

    Implement password validation and comparison
    Added password validation and comparison functions.

--- script.js
+++ script.js
@@ -11,3 +11,34 @@
             document.getElementById("todayDate").innerHTML =
                 today.toLocaleDateString("en-US", options);
         }
+}
+        function validatePassword() {
+
+        let pass=document.getElementById("password").value;
+
+        let regex= /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+=<>.?/-]).{8,30}$/;
+        
+        if(!regex.test(pass))
+        {
+        alert("Password requirements not met.");
+        }
+        <input
+                type="password"
+                id="password"
+                onchange="validatePassword()">
+
+        function comparePasswords(){
+                let p1=document.getElementById("password").value;
+
+                let p2=document.getElementById("password2").value;
+
+                if(p1!=p2)
+                {
+                alert("Passwords do not match.");
+                }
+
+}
+                <input
+                type="password"
+                id="password2"
+                onchange="comparePasswords()">
