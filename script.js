e711900fded94adf14bbe26ef2bd138cddf11bd8
gtran2003 <gtran2@cougarnet.uh.edu>
2026-06-28 16:55:41 -0500

    Add function to display formatted current date

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+        function displayDate() {
+            var today = new Date();
 
+            var options = {
+                weekday: 'long',
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric'
+            };
+
+            document.getElementById("todayDate").innerHTML =
+                today.toLocaleDateString("en-US", options);
+        }
